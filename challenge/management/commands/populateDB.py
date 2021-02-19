e35c80d1699e358dd0e619f4fab88be315cf0772
matheus <matheus@tradenamao.com.br>
2021-02-18 22:30:56 -0300

    implement solid practices

--- challenge/management/commands/populateDB.py
+++ challenge/management/commands/populateDB.py
@@ -8,42 +8,63 @@ import datetime, json, requests, csv
  
 class Command(BaseCommand):
 
+    def __instance_file(self):
+        csv_file = open('challenge/utils/athlete_events.csv')
+        reader = csv.DictReader(csv_file, delimiter=',')
+        return reader
+
+    def __check_line_value(self, line):
+        for features in ['Age', 'Height', 'Weight', 'Medal']:
+            line[features] = None if line[features] == 'NA' else line[features]
+        return line
+
+    def __save_data(self, line):
+        athlete = self.__create_athlete(line)
+        event = self.__create_event(line)
+        athlete_info = self.__create_athlete_info(line, athlete)
+        athlete_info.event.add(event)
+
+
+    def __create_athlete(self, line):
+        athlete, created = Athlete.objects.get_or_create(
+            athlete_name=line['Name'].lstrip(),
+        )
+        return athlete
+
+    def __create_event(self, line):
+        event, created = Event.objects.get_or_create(
+            event_name=line['Event'].lstrip(),
+            city=line['City'],
+            sport=line['Sport'],
+            season=line['Season'],
+            year=line['Year'],
+            games=line['Games'],
+        )
+        return event
+
+    def __create_athlete_info(self, line, athlete):
+        athlete_info, created = AthleteInfo.objects.get_or_create(
+            athlete=athlete,
+            sex=line['Sex'],
+            age=line['Age'],
+            height=line['Height'],
+            weight=line['Weight'],
+            team=line['Team'],
+            medal=line['Medal'],
+        )
+        return athlete_info
+
+
     def handle(self, *args, **options):
 
         # Get local csv
-        csv_file = open('challenge/utils/athlete_events.csv')
-        reader = csv.DictReader(csv_file, delimiter=',')
+        reader = self.__instance_file()
 
         print("Reading CSV file...\n")
         for i, line in enumerate(reader):
             
             print(f'{i}/271116', end='\r', flush=True)
+            line = self.__check_line_value(line)
+            self.__save_data(line)
             
-            for features in ['Age', 'Height', 'Weight', 'Medal']:
-                line[features] = None if line[features] == 'NA' else line[features]
-
-            athlete, created = Athlete.objects.get_or_create(
-                athlete_name=line['Name'].lstrip(),
-            )
-
-            event, created = Event.objects.get_or_create(
-                event_name=line['Event'].lstrip(),
-                city=line['City'],
-                sport=line['Sport'],
-                season=line['Season'],
-                year=line['Year'],
-                games=line['Games'],
-            )
-
-            athlete_info, created = AthleteInfo.objects.get_or_create(
-                athlete=athlete,
-                sex=line['Sex'],
-                age=line['Age'],
-                height=line['Height'],
-                weight=line['Weight'],
-                team=line['Team'],
-                medal=line['Medal'],
-            )
-            athlete_info.event.add(event)
-
         print("Process completed successfully")
